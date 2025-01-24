96e74b0a98841b254971b8aa47fcf5eb6098eb66
Hugo Gresse <hugo.gresse@gmail.com>
2025-01-24 15:04:50 +0100

    Add hello world

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ const fastify = Fastify({
   logger: true,
 });
 
+fastify.get("/", function (request, reply) {
+  reply.send("Hello World");
+});
 fastify.get("/api/movies", getApiMovies);
 fastify.get("/api/watchlist", getApiWatchlist);
 fastify.post("/api/watchlist", postAddToWatchlist);
 fastify.delete("/api/watchlist", postRemoveFromWatchlist);
-fastify.listen({ port: process.env.PORT }, function (err) {
+fastify.listen({ port: process.env.PORT || 3000 }, function (err) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
